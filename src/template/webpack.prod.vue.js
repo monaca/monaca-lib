fcf998b24bee1aa04db8c56b0abbd6813da14de5
adamkozuch <adam.kozuch@gmail.com>
2017-03-17 14:19:29 +0900

    remove stylus loader

--- src/template/webpack.prod.vue.js
+++ src/template/webpack.prod.vue.js
@@ -68,9 +68,6 @@ module.exports = {
       test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
       loader: 'file?name=assets/[name].[hash].[ext]'
     }, {
-      test: /\.styl$/,
-      loader: 'style!css!postcss!stylus'
-    }, {
       test: /\.css$/,
       include: [/\/onsen-css-components.css$/, path.join(__dirname, 'src')],
       loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1&-raw!postcss')
