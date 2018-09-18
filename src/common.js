bdec369faf1c4789b7282ae977deb229c608b010
yong <yong@asial.co.jp>
2018-09-18 14:33:08 +0900

    add doc url for update project

--- src/common.js
+++ src/common.js
@@ -4,6 +4,13 @@ const migrationDocUrl = () => {
 	return `https://docs.monaca.io/${language}/products_guide/migration`;
 }
 
+const updateDocUrl = () => {
+	let language = 'en';
+	if (global.locale === 'ja' || process.env.LOCALE === 'ja') language = 'ja';
+	return `https://docs.monaca.io/${language}/release_notes/20180918_monaca_cli_3.0/`;
+}
+
 module.exports = {
-  migrationDocUrl
+  migrationDocUrl,
+  updateDocUrl
 };
