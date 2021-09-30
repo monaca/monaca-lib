47340f88bdd614509ec8b9db145eb6365780bd17
yong <yong@asial.co.jp>
2021-09-30 14:05:47 +0900

    change docs_url

--- src/common.js
+++ src/common.js
@@ -1,13 +1,13 @@
 const migrationDocUrl = () => {
 	let language = 'en';
 	if (global.locale === 'ja' || process.env.LOCALE === 'ja') language = 'ja';
-	return `https://docs.monaca.io/${language}/products_guide/migration`;
+	return `https://${language}.docs.monaca.io/products_guide/migration`;
 }
 
 const updateDocUrl = () => {
 	let language = 'en';
 	if (global.locale === 'ja' || process.env.LOCALE === 'ja') language = 'ja';
-	return `https://docs.monaca.io/${language}/release_notes/20180918_monaca_cli_3.0/`;
+	return `https://${language}.docs.monaca.io/release_notes/20180918_monaca_cli_3.0/`;
 }
 
 module.exports = {
