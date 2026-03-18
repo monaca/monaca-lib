e00a5c6c34f5bb0d85be28293694cce31f52312c
knaito-asial <knaito@asial.co.jp>
2026-03-18 21:14:32 +0900

    fix: fix templates test

--- spec/monaca.templates.spec.js
+++ spec/monaca.templates.spec.js
@@ -10,8 +10,13 @@
   describe('Templates Test', function() {
     it('should return a list of templates', function(done) {
       monaca.getTemplates().then(
-        function(templates) {
+        function(results) {
+          const templates = results.template;
+          const samples = results.sample;
+          const tags = results.tags;
           expect(templates.length > 5).toBeTruthy();  
+          expect(samples.length > 5).toBeTruthy();  
+          expect(tags.length > 5).toBeTruthy();  
           done();
         }
       );
